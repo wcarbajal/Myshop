8bcb055d09ee4d435eb18f0d78417616d189c267
wuilmer carbajal <wcarbajal@outlook.com>
2025-12-02 15:47:01 -0500

    correccion de vista de imagen en cell 3

--- src/components/product/product-image/ViewImage.tsx
+++ src/components/product/product-image/ViewImage.tsx
@@ -3,23 +3,23 @@ import Image from 'next/image';
 interface Props {
   src?: string;
   alt: string;
-  className?: React.StyleHTMLAttributes<HTMLImageElement>['className'];
-  style?: React.StyleHTMLAttributes<HTMLImageElement>['style'];
+  className?: React.StyleHTMLAttributes<HTMLImageElement>[ 'className' ];
+  style?: React.StyleHTMLAttributes<HTMLImageElement>[ 'style' ];
   width: number;
   height: number;
 }
 
-export const ViewImage = ({
+export const ViewImage = ( {
   src,
   alt,
   className,
   style,
   width,
   height
-}: Props) => {
+}: Props ) => {
 
-  const localSrc = ( src ) 
-    ? src.startsWith('http') // https://urlcompletodelaimagen.jpg
+  const localSrc = ( src )
+    ? src.startsWith( 'http' ) // https://urlcompletodelaimagen.jpg
       ? src
       : `/products/${ src }`
     : '/imgs/placeholder.jpg';
@@ -28,12 +28,12 @@ export const ViewImage = ({
     <Image
       src={ localSrc }
       width={ width }
-      height={ height}
+      height={ height }
       alt={ alt }
       className={ className }
       style={ style }
-      priority={true}
-      unoptimized={true}
+      priority={ true }
+      unoptimized={ true }
       loading="eager"
       sizes="(max-width: 640px) 50vw, (max-width: 768px) 33vw, (max-width: 1024px) 25vw, 20vw"
     />
